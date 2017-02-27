0f026c9c1b733e4bda4c5a77d40d487061486b7b
Ivan Dmytryna <admin@Admins-MacBook-Pro.local>
2017-02-27 09:49:21 +0200

    Fixed tracking area behavior

--- BBLBasics/TrackingAreaOwner.swift
+++ BBLBasics/TrackingAreaOwner.swift
@@ -13,18 +13,14 @@ import AppKit
 /// allows creating tracking areas with closures.
 public class TrackingAreaOwner: NSView {
   let handlers: [NSEventType:()->Void]
-  
+  var trackingArea: NSTrackingArea?
   convenience public init?(view: NSView, handlers: [NSEventType:()->Void]) {
     self.init(coder: NSArchiver(), handlers: handlers)
-    
-    let rect = view.bounds  // superseded by .inVisibleRect
-    let options: NSTrackingAreaOptions = [.inVisibleRect, .mouseEnteredAndExited, .activeAlways]
-    let area = NSTrackingArea(rect: rect, options: options, owner: self, userInfo:nil)
-    view.addTrackingArea(area)
   }
   
   init?(coder: NSCoder, handlers: [NSEventType:()->Void]) {
     self.handlers = handlers
+    self.trackingArea = nil
     super.init(frame: .zero)
   }
   
@@ -32,6 +28,15 @@ public class TrackingAreaOwner: NSView {
     fatalError("init(coder:) has not been implemented")
   }
   
+  override public func updateTrackingAreas() {
+    let rect = self.bounds  // superseded by .inVisibleRect
+    let options: NSTrackingAreaOptions = [.inVisibleRect, .mouseEnteredAndExited, .activeAlways]
+    if self.trackingArea != nil {
+      self.removeTrackingArea(trackingArea!)
+    }
+    self.trackingArea = NSTrackingArea(rect: rect, options: options, owner: self, userInfo:nil)
+    self.addTrackingArea(self.trackingArea!)
+  }
   override public func mouseEntered(with event: NSEvent) {
     maybeInvokeHandler(event.type)
   }
