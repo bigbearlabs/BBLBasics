4509cc8f35ccaaedee87816f58e21af1d49015ca
Andy Park <andy@bigbearlabs.com>
2021-06-29 03:23:37 +0900

    move latest status item tech up to lib.

--- BBLBasics/StatusItemController.swift
+++ BBLBasics/StatusItemController.swift
@@ -1,9 +1,11 @@
-open class StatusItemController {
+open class StatusItemController: NSObject { // for nib-based menu loading.
   
-  let image: NSImage
+  let image: NSImage?
   
-  let onClick: () -> ()
-  
+  @IBOutlet public var menu: NSMenu?
+
+  var onClick: () -> Void = {}
+  let onRightClick: () -> Void
   
   private lazy var statusItem: NSStatusItem = {
     let item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
@@ -12,37 +14,57 @@ open class StatusItemController {
     
     item.button?.target = self
     item.button?.action = #selector(action_buttonClicked(_:))
-    
-    item.highlightMode = true
+    item.button?.sendAction(on: [.leftMouseDown, .rightMouseDown])
+
+    // ensure no menu on item, to allow right-clicks.
+    item.menu = nil
     
     return item
   }()
   
   
-  public init(image: NSImage, onClick: @escaping () -> ()) {
-    
+  public init(
+    image: NSImage?,
+    menu: NSMenu? = nil,
+    onClick: (() -> Void)? = nil,
+    onRightClick: @escaping () -> Void = {}) {
+        
     self.image = image
+    self.menu = menu
+    self.onRightClick = onRightClick
+    
+    super.init()
+
     self.onClick = onClick
+      ?? { [unowned self] in
+        if let menu = self.menu {
+          self.statusItem.popUpMenu(menu)
+        }
+      }
     
     _ = self.statusItem
   }
   
-  public var menu: NSMenu? {
-    get {
-      return self.statusItem.menu
-    }
-    set {
-      self.statusItem.menu = newValue
-    }
-  }
   
   @IBAction
   func action_buttonClicked(_ sender: NSButton) {
-    onClick()
+
+    guard let currentEvent = NSApp.currentEvent else {
+        return
+    }
+
+    switch currentEvent.type {
+    case .leftMouseDown:
+      onClick()
+      
+    case .rightMouseDown:
+      onRightClick()
+      
+    default: ()
+    }
+
   }
-  
-  
-  
+
   
   public var view: NSView? {
     return self.statusItem.button
